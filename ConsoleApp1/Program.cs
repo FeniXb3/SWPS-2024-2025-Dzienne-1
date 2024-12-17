9a498c772ac376d7859931d6eae75c4ce3ce7840
Konrad Gadzina <konrad@enklawa.edu.pl>
2024-12-17 15:37:54 +0100

    Init 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Jakie słowo ma być odgadnięte?");
+string fullText = Console.ReadLine();
+
+Console.WriteLine("Ile szans ma mieć osoba odgadująca?");
+// int lives = int.Parse(Console.ReadLine());
+// int lives = Convert.ToInt32(Console.ReadLine());
+// int lives;
+// int.TryParse(Console.ReadLine(), out lives);
+if (!int.TryParse(Console.ReadLine(), out int lives))
+{
+    lives = 3;
+    Console.WriteLine($"Niepoprawna wartość, ustalam liczbę szans na {lives}");
+}
+
+
+// Poniższa linijka wywali grę, jeśli uruchomi się ją w Debug Console, dlatego zakomentowałem
+// Console.Clear();
+string[] knownLetters = new string[fullText.Length];
+Array.Fill(knownLetters, "-");
+
+Random rng = new Random();
+int index = rng.Next(fullText.Length);
+string preguestLetter = fullText[index].ToString();
+int start = 0;
+while (true)
+{
+    int i = fullText.IndexOf(preguestLetter, start);
+    if (i == -1)
+    {
+        break;
+    }
+
+    knownLetters[i] = preguestLetter;
+    start = i + 1;
+}
+
+string knownText = string.Join(string.Empty, knownLetters);
+
+while (lives > 0 && knownText != fullText)
+{
+    Console.WriteLine($"Known text: {knownText}");
+    Console.WriteLine("Guess a letter:");
+    string letter = Console.ReadLine();
+
+    if (fullText.Contains(letter))
+    {
+        for (int i = 0; i < knownLetters.Length; i++)
+        {
+            if (knownLetters[i] == "-")
+            {
+                if (fullText[i].ToString() == letter)
+                {
+                    Console.WriteLine("Tak, ta litera znajduje się w tekście!");
+                    knownLetters[i] = letter;
+                }
+            }
+        }
+        knownText = string.Join(string.Empty, knownLetters);
+    }
+    else
+    {
+        Console.WriteLine("Litera, której szukasz jest w innym zamku");
+        lives -= 1;
+        //lives = lives - 1;
+        // lives--;
+    }
+
+    // if (lives <= 0)
+    // {
+    //     break;
+    // }
+}
+
+if (lives > 0)
+{
+    Console.WriteLine($"Tak, tekst to {knownText}");
+}
+else
+{
+    Console.WriteLine($"Starałeś się starałeś, a i tak przegrałeś. Poprawny tekst to {fullText}");
+}
---
+++ RPS/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+const string firstAllowedSign = "fire";
+const string secondAllowedSign = "water";
+const string thirdAllowedSign = "grass";
+
+string[] allowedSigns = ["fire", "water", "grass"];
+int firstPlayerPoints = 0;
+int secondPlayerPoints = 0;
+
+while (true)
+{
+    // Console.WriteLine("Podaj znak, graczu 1 (" + firstAllowedSign + "/" + secondAllowedSign + "/" + thirdAllowedSign + ")");
+    // Console.WriteLine($"Podaj znak, graczu 1 ({allowedSigns[0]}/{allowedSigns[1]}/{allowedSigns[2]})");
+    Console.WriteLine($"Podaj znak, graczu 1 ({string.Join("/", allowedSigns)})");
+    // typ nazwa; 
+    // typ nazwa = wartość;
+
+    // string firstSign = (Console.ReadLine() ?? string.Empty).ToLower();
+    string firstSign = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
+    // firstSign = firstSign.ToLower();
+
+    // while (firstSign != firstAllowedSign && firstSign != secondAllowedSign && firstSign != thirdAllowedSign)
+    while (!allowedSigns.Contains(firstSign))
+    {
+        Console.WriteLine("Niepoprawny znak!");
+        // Console.WriteLine($"Podaj POPRAWNY znak, graczu 1 ({firstAllowedSign}/{secondAllowedSign}/{thirdAllowedSign})");
+        Console.WriteLine($"Podaj POPRAWNY znak, graczu 1 ({string.Join("/", allowedSigns)})");
+        firstSign = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
+    }
+
+    // Console.WriteLine($"Podaj znak, graczu 2 ({firstAllowedSign}/{secondAllowedSign}/{thirdAllowedSign})");
+    Console.WriteLine($"Podaj znak, graczu 2 ({string.Join("/", allowedSigns)})");
+    string secondSign = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
+
+
+    // while (secondSign != firstAllowedSign && secondSign != secondAllowedSign && secondSign != thirdAllowedSign)
+    while (!allowedSigns.Contains(secondSign))
+    {
+        Console.WriteLine("Niepoprawny znak!");
+        // Console.WriteLine($"Podaj POPRAWNY znak, graczu 2 ({firstAllowedSign}/{secondAllowedSign}/{thirdAllowedSign})");
+        Console.WriteLine($"Podaj POPRAWNY znak, graczu 2 ({string.Join("/", allowedSigns)})");
+        secondSign = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
+    }
+
+    // porównanie znaków - instrukcja warunkowa if
+    if (firstSign == secondSign) // dlaczego tutaj nie ma średnika?!?!?!
+    {
+        Console.WriteLine("Remis!");
+    }
+    else if ((firstSign == firstAllowedSign && secondSign == thirdAllowedSign) || (firstSign == secondAllowedSign && secondSign == firstAllowedSign) || (firstSign == thirdAllowedSign && secondSign == secondAllowedSign))
+    {
+        Console.WriteLine("Gracz 1 MISZCZ!");
+        // firstPlayerPoints = firstPlayerPoints + 1;
+        firstPlayerPoints += 1;
+    }
+    else
+    {
+        Console.WriteLine("Gracz 2 MISZCZ!");
+        // secondPlayerPoints = secondPlayerPoints + 1;
+        secondPlayerPoints += 1;
+    }
+
+    Console.WriteLine($"Punkty gracza 1: {firstPlayerPoints}");
+    Console.WriteLine($"Punkty gracza 2: {secondPlayerPoints}");
+
+    Console.WriteLine("Czy kończymy na dziś?");
+    string reply = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
+    if (reply == "tak")
+    {
+        break;
+    }
+}
+
+Console.WriteLine("Dziękuję za grę!");
